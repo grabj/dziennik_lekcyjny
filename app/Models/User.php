5ce0cbef92a660e4c9bd9664f36aceea9fca06f5
Justyna G <96183391+grabj@users.noreply.github.com>
2025-01-10 00:31:55 +0100

    register bug fix
    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = ['name', 'surname', 'email', 'password', 'role'];
 
+    protected $attributes = [
+        'role' => 'unsigned'
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
